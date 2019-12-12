03e403aa74718d41a6ccdfc3252673a49aaa5a49
ztiany <ztiany3@gmail.com>
2019-12-13 00:32:26 +0800

    update

--- C&CPP/00-Code/cpp-program-basic/07-oop/friend_class.h
+++ C&CPP/00-Code/cpp-program-basic/07-oop/friend_class.h
@@ -13,14 +13,27 @@
 #include <iostream>
 #include <vector>
 
+//先声明Box。
+class Box;
+
+class FriendMethod {
+public:
+    //这里只要声明，不要定义，否则无法访问Box的成员。
+    void printBoxInfo(Box &box);
+};
+
 class Box {
 private:
     double width;
 public:
     void setWidth(double wid);
+
 public://友元放在一起
-    friend void printWidth(Box box);
-    friend class Friend;
+    friend class Friend;//友元类
+
+    friend void FriendMethod::printBoxInfo(Box &);//友元成员函数
+
+    friend void printWidth(Box box);//普通友元函数
 };
 
 // 成员函数定义
@@ -41,5 +54,35 @@ public:
         return box.width;
     }
 };
+
+//定义友元成员函数
+void FriendMethod::printBoxInfo(Box &box) {
+    std::cout << "box: " << box.width << std::endl;
+}
+
+//演示友元的作用域
+class FriendScope{
+public:
+    //可以在类中直接定义友元，但是如果想要调用该函数，还需要用普通方式再声明一次
+    friend void f(){}
+    FriendScope(){
+        //错误的调用，f 没有被声明。
+        //f();
+    }
+
+    void g();
+    void h();
+};
+
+void FriendScope::g(){
+    //错误的调用，f 没有被声明。
+    //f();
+}
+
+void f();
+
+void FriendScope::h(){
+    f();//可以调用，因为 f 已经声明在作用域中了。
+}
 
 #endif //C_BASIC_FRIEND_CLASS_H
