de0348b61626123e7e7e52d99b0e7c2ae7321f8b
Ztiany <Ztiany3@gmail.com>
2019-12-23 19:31:56 +0800

    update

--- C&CPP/00-Code/c-program-basic/01-Basic/InputOutput.c
+++ C&CPP/00-Code/c-program-basic/01-Basic/InputOutput.c
@@ -21,7 +21,7 @@ static void getcharEOF() {
     }
 }
 
-static void scanfReturn(){
+static void scanfReturn() {
     int result;
     //当用户输入的不是数字，ret=0
     int ret = scanf("%d", &result);
@@ -29,7 +29,7 @@ static void scanfReturn(){
     printf("result = %d\n", result);
 }
 
-static void printfSample1(){
+static void printfSample1() {
     printf("%-20s\n", "a");//指定打印宽度，并左对齐
     printf("%20s\n", "a");//指定打印宽度，并右对齐
     printf("%+d\n", 32);//添加正负符号
@@ -39,17 +39,27 @@ static void printfSample1(){
     printf("%o\n", 32);//转为八进制
 }
 
-static void scanfSample2(){
-    char result [20];
+static void scanfSample2() {
+    char result[20];
     //指定值读取10个字符
     scanf("%10s", result);
     printf("%s\n", result);
 }
 
+static void sscanfSample1() {
+    //https://www.cnblogs.com/lyq105/archive/2009/11/28/1612677.html
+    char *buffer = "ae812000-ae8a8000 r--s 00bbf000 fd:20 8276       /data/app/com.bhcredit.app-2/base.apk";
+    char path[256];
+    sscanf(buffer, "%*llx-%*llx %*s %*s %*s %*s %s", path);
+    //output: result = /data/app/com.bhcredit.app-2/base.apk
+    printf("result = %s", path);
+}
+
 int main() {
     //getcharEOF();
-    scanfReturn();
+    //scanfReturn();
     //printfSample1();
     //scanfSample2();
+    sscanfSample1();
     return EXIT_SUCCESS;
 }
---
+++ C&CPP/00-Code/cpp-program-basic/07-oop/static_filed.h
@@ -0,0 +1,13 @@
+/*
+ ============================================================================
+ 
+ Author      : Ztiany
+ Description : 静态成员
+
+ ============================================================================
+ */
+
+#ifndef CPLUSPLUS_PROGRAM_STATIC_FILED_H
+#define CPLUSPLUS_PROGRAM_STATIC_FILED_H
+
+#endif //CPLUSPLUS_PROGRAM_STATIC_FILED_H
