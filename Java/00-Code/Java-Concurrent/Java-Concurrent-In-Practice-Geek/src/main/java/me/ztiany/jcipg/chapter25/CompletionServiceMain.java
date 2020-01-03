f7890188040b5726ae7065b6ada33005470c934b
Ztiany <Ztiany3@gmail.com>
2020-01-03 16:42:17 +0800

    update java

--- Java/00-Code/Java-Concurrent/Java-Concurrent-In-Practice-Geek/src/main/java/me/ztiany/jcipg/chapter25/CompletionServiceMain.java
+++ Java/00-Code/Java-Concurrent/Java-Concurrent-In-Practice-Geek/src/main/java/me/ztiany/jcipg/chapter25/CompletionServiceMain.java
@@ -1,11 +1,18 @@
 package me.ztiany.jcipg.chapter25;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Ztiany
@@ -15,21 +22,157 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class CompletionServiceMain {
 
     public static void main(String... args) throws ExecutionException, InterruptedException {
-        sample1();
-        sample2();
+        //sample1();
+        //sample2();
+        //sample3();
+        //sample4();
+        //sample5();
+        question();
+    }
+
+    private static void question() {
+        // 创建线程池
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        // 创建CompletionService
+        CompletionService<Integer> cs = new ExecutorCompletionService<>(executor);
+        // 异步向电商S1询价
+        cs.submit(() -> getPriceByS1());
+        // 异步向电商S2询价
+        cs.submit(() -> getPriceByS2());
+        // 异步向电商S3询价
+        cs.submit(() -> getPriceByS3());
+        // 将询价结果异步保存到数据库
+        // 并计算最低报价
+        AtomicReference<Integer> m = new AtomicReference<>(Integer.MAX_VALUE);
+        for (int i = 0; i < 3; i++) {
+            executor.execute(() -> {
+                Integer r = null;
+                try {
+                    r = cs.take().get();
+                } catch (Exception e) {
+                }
+                save(r);
+                m.set(Integer.min(m.get(), r));
+            });
+        }
+
+        System.out.println("result = " + m);
+    }
+
+    private static void sample5() {
+        // 创建线程池
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        // 创建CompletionService
+        CompletionService<Integer> cs = new ExecutorCompletionService<>(executor);
+        // 用于保存Future对象
+        List<Future<Integer>> futures = new ArrayList<>(3);
+        //提交异步任务，并保存future到futures
+        futures.add(cs.submit(() -> geocoderByS1()));
+        futures.add(cs.submit(() -> geocoderByS2()));
+        futures.add(cs.submit(() -> geocoderByS3()));
+        // 获取最快返回的任务执行结果
+        Integer r = 0;
+        try {
+            // 只要有一个成功返回，则break
+            for (int i = 0; i < 3; ++i) {
+                r = cs.take().get();
+                //简单地通过判空来检查是否成功返回
+                if (r != null) {
+                    break;
+                }
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            //取消所有任务
+            for (Future<Integer> f : futures) {
+                f.cancel(true);
+            }
+        }
+
+        // 返回结果
+        System.out.println("result = " + r);
+    }
+
+    private static Integer geocoderByS1() throws InterruptedException {
+        Thread.sleep(1700);
+        return 1;
+    }
+
+    private static Integer geocoderByS2() throws InterruptedException {
+        Thread.sleep(1070);
+        return 2;
+    }
+
+    private static Integer geocoderByS3() throws InterruptedException {
+        Thread.sleep(1040);
+        return 3;
+    }
+
+    private static void sample3() throws InterruptedException {
+        // 创建阻塞队列
+        final BlockingQueue<Integer> bq = new LinkedBlockingQueue<>();
+        //执行器
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        class Task extends FutureTask<Integer> {
+            private Task(Callable<Integer> callable) {
+                super(callable);
+            }
+
+            @Override
+            protected void done() {
+                try {
+                    bq.put(get());
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // 异步向电商S1询价
+        executor.submit(new Task(() -> getPriceByS1()));
+        // 异步向电商S2询价
+        executor.submit(new Task(() -> getPriceByS2()));
+        // 异步向电商S3询价
+        executor.submit(new Task(() -> getPriceByS3()));
+
+        //异步保存所有报价
+        for (int i = 0; i < 3; i++) {
+            Integer r = bq.take();
+            executor.execute(() -> save(r));
+        }
+    }
+
+    private static void sample4() throws InterruptedException, ExecutionException {
+        // 创建线程池
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        // 创建CompletionService
+        CompletionService<Integer> cs = new ExecutorCompletionService<>(executor);
+        // 异步向电商S1询价
+        cs.submit(() -> getPriceByS1());
+        // 异步向电商S2询价
+        cs.submit(() -> getPriceByS2());
+        // 异步向电商S3询价
+        cs.submit(() -> getPriceByS3());
+        // 将询价结果异步保存到数据库
+        for (int i = 0; i < 3; i++) {
+            Integer r = cs.take().get();
+            executor.execute(() -> save(r));
+        }
     }
 
     private static void sample2() throws InterruptedException {
         // 创建线程池
         ExecutorService executor = Executors.newFixedThreadPool(3);
         // 异步向电商S1询价
-        Future<String> f1 = executor.submit(CompletionServiceMain::getPriceByS1);
+        Future<Integer> f1 = executor.submit(() -> getPriceByS1());
         // 异步向电商S2询价
-        Future<String> f2 = executor.submit(CompletionServiceMain::getPriceByS2);
-        // 异步向电商S3询价
-        Future<String> f3 = executor.submit(CompletionServiceMain::getPriceByS3);
+        Future<Integer> f2 = executor.submit(() -> getPriceByS2());
+        // 异步向电商S3询价FutureTask
+        Future<Integer> f3 = executor.submit(() -> getPriceByS3());
         // 创建阻塞队列
-        BlockingQueue<String> bq = new LinkedBlockingQueue<>();
+        BlockingQueue<Integer> bq = new LinkedBlockingQueue<>();
         //电商S1报价异步进入阻塞队列
         executor.execute(() -> {
             try {
@@ -57,7 +200,7 @@ public class CompletionServiceMain {
 
         //异步保存所有报价
         for (int i = 0; i < 3; i++) {
-            String r = bq.take();
+            Integer r = bq.take();
             executor.execute(() -> save(r));
         }
     }
@@ -66,39 +209,41 @@ public class CompletionServiceMain {
         // 创建线程池
         ExecutorService executor = Executors.newFixedThreadPool(3);
         // 异步向电商S1询价
-        Future<String> f1 = executor.submit(CompletionServiceMain::getPriceByS1);
+        Future<Integer> f1 = executor.submit(CompletionServiceMain::getPriceByS1);
         // 异步向电商S2询价
-        Future<String> f2 = executor.submit(CompletionServiceMain::getPriceByS2);
+        Future<Integer> f2 = executor.submit(CompletionServiceMain::getPriceByS2);
         // 异步向电商S3询价
-        Future<String> f3 = executor.submit(CompletionServiceMain::getPriceByS3);
+        Future<Integer> f3 = executor.submit(CompletionServiceMain::getPriceByS3);
 
         // 获取电商S1报价并保存
-        final String r1 = f1.get();
+        final Integer r1 = f1.get();
         executor.execute(() -> save(r1));
 
         // 获取电商S2报价并保存
-        final String r2 = f2.get();
+        final Integer r2 = f2.get();
         executor.execute(() -> save(r2));
 
         // 获取电商S3报价并保存
-        final String r3 = f3.get();
+        final Integer r3 = f3.get();
         executor.execute(() -> save(r3));
     }
 
-    private static String getPriceByS1() throws InterruptedException {
+    private static Integer getPriceByS1() throws InterruptedException {
         Thread.sleep(1000);
-        return "P1";
+        return 1;
     }
 
-    private static String getPriceByS2() {
-        return "P2";
+    private static Integer getPriceByS2() throws InterruptedException {
+        Thread.sleep(1700);
+        return 2;
     }
 
-    private static String getPriceByS3() {
-        return "P3";
+    private static Integer getPriceByS3() throws InterruptedException {
+        Thread.sleep(1400);
+        return 3;
     }
 
-    private static void save(String s) {
+    private static void save(Integer s) {
         System.out.println("save: " + s);
     }
 
--- Java/05-Concurrent/05-极客时间《Java并发编程实战》/17-CompletionService.md
+++ Java/05-Concurrent/05-极客时间《Java并发编程实战》/17-CompletionService.md
@@ -36,11 +36,11 @@
         // 创建线程池
         ExecutorService executor = Executors.newFixedThreadPool(3);
         // 异步向电商S1询价
-        Future<String> f1 = executor.submit(CompletionServiceMain::getPriceByS1);
+        Future<String> f1 = executor.submit(() -> getPriceByS1());
         // 异步向电商S2询价
-        Future<String> f2 = executor.submit(CompletionServiceMain::getPriceByS2);
+        Future<String> f2 = executor.submit(() -> getPriceByS2());
         // 异步向电商S3询价
-        Future<String> f3 = executor.submit(CompletionServiceMain::getPriceByS3);
+        Future<String> f3 = executor.submit(() -> getPriceByS3());
         // 创建阻塞队列
         BlockingQueue<String> bq = new LinkedBlockingQueue<>();
         //电商S1报价异步进入阻塞队列
@@ -75,6 +75,43 @@
         }
 ```
 
+>这里感觉其实还可以使用 FutureTask，通过实现 FutureTask 的 done 方法可以监听任务执行完成。
+
+```cpp
+  // 创建阻塞队列
+        final BlockingQueue<String> bq = new LinkedBlockingQueue<>();
+        //执行器
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        class Task extends FutureTask<String> {
+            private Task(Callable<String> callable) {
+                super(callable);
+            }
+
+            @Override
+            protected void done() {
+                try {
+                    bq.put(get());
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // 异步向电商S1询价
+        executor.submit(new Task(() -> getPriceByS1()));
+        // 异步向电商S2询价
+        executor.submit(new Task(() -> getPriceByS2()));
+        // 异步向电商S3询价
+        executor.submit(new Task(() -> getPriceByS3()));
+
+        //异步保存所有报价
+        for (int i = 0; i < 3; i++) {
+            String r = bq.take();
+            executor.execute(() -> save(r));
+        }
+```
+
 ## 2 使用 CompletionService 实现询价系统
 
 针对上面使用阻塞队列的优化，在实际项目中，是不建议的，因为 Java SDK 并发包里已经提供了设计精良的 CompletionService。利用 CompletionService 不但能帮你解决先获取到的报价先保存到数据库的问题，而且还能让代码更简练。
@@ -91,11 +128,134 @@ ExecutorCompletionService(Executor executor)；
 ExecutorCompletionService(Executor executor, BlockingQueue> completionQueue)。
 ```
 
+这两个构造方法都需要传入一个线程池，如果不指定 completionQueue，那么默认会使用无界的 LinkedBlockingQueue。任务执行结果的 Future 对象就是加入到 completionQueue 中。
+
+下面的示例代码完整地展示了如何利用 CompletionService 来实现高性能的询价系统：
+
+```java
+        // 创建线程池
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        // 创建CompletionService
+        CompletionService<String> cs = new ExecutorCompletionService<>(executor);
+        // 异步向电商S1询价
+        cs.submit(() -> getPriceByS1());
+        // 异步向电商S2询价
+        cs.submit(() -> getPriceByS2());
+        // 异步向电商S3询价
+        cs.submit(() -> getPriceByS3());
+        // 将询价结果异步保存到数据库
+        for (int i = 0; i < 3; i++) {
+            String r = cs.take().get();
+            executor.execute(() -> save(r));
+        }
+```
+
 ## 3 CompletionService 详解
+
+CompletionService 接口提供的方法有 5 个，这 5 个方法的方法签名如下所示：
+
+```java
+Future<V> submit(Callable<V> task);
+Future<V> submit(Runnable task, V result);
+Future<V> take() throws InterruptedException;
+Future<V> poll();
+Future<V> poll(long timeout, TimeUnit unit)throws InterruptedException;
+```
+
+- submit() 相关的方法有两个，用于提交任务。
+- 其余的 3 个方法，都是和阻塞队列相关的，take()、poll() 都是从阻塞队列中获取并移除一个元素；它们的区别在于如果阻塞队列是空的，那么调用 take() 方法的线程会被阻塞，而 poll() 方法会返回 null 值。 poll(long timeout, TimeUnit unit) 方法支持以超时的方式获取并移除阻塞队列头部的一个元素，如果等待了 timeout unit 时间，阻塞队列还是空的，那么该方法会返回 null 值。
 
 ## 4 利用 CompletionService 实现 Dubbo 中的 Forking Cluster
 
+Dubbo 中有一种叫做 Forking 的集群模式，这种集群模式下，支持并行地调用多个查询服务，只要有一个成功返回结果，整个服务就可以返回了。例如你需要提供一个地址转坐标的服务，为了保证该服务的高可用和性能，你可以并行地调用 3 个地图服务商的 API，然后只要有 1 个正确返回了结果 r，那么地址转坐标这个服务就可以直接返回 r 了。这种集群模式可以容忍 2 个地图服务商服务异常，但缺点是消耗的资源偏多。
+
+```java
+geocoder(addr) {
+  //并行执行以下3个查询服务，
+  r1=geocoderByS1(addr);
+  r2=geocoderByS2(addr);
+  r3=geocoderByS3(addr);
+  //只要r1,r2,r3有一个返回
+  //则返回
+  return r1|r2|r3;
+}
+```
+
+利用 CompletionService 可以快速实现 Forking 这种集群模式，比如下面的示例代码就展示了具体是如何实现的。首先我们创建了一个线程池 executor 、一个 CompletionService 对象 cs 和一个Future类型的列表 futures，每次通过调用 CompletionService 的 submit() 方法提交一个异步任务，会返回一个 Future 对象，我们把这些 Future 对象保存在列表 futures 中。通过调用 cs.take().get()，我们能够拿到最快返回的任务执行结果，只要我们拿到一个正确返回的结果，就可以取消所有任务并且返回最终结果了。
+
+```java
+        // 创建线程池
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        // 创建CompletionService
+        CompletionService<Integer> cs = new ExecutorCompletionService<>(executor);
+        // 用于保存Future对象
+        List<Future<Integer>> futures = new ArrayList<>(3);
+        //提交异步任务，并保存future到futures
+        futures.add(cs.submit(() -> geocoderByS1()));
+        futures.add(cs.submit(() -> geocoderByS2()));
+        futures.add(cs.submit(() -> geocoderByS3()));
+        // 获取最快返回的任务执行结果
+        Integer r = 0;
+        try {
+            // 只要有一个成功返回，则break
+            for (int i = 0; i < 3; ++i) {
+                r = cs.take().get();
+                //简单地通过判空来检查是否成功返回
+                if (r != null) {
+                    break;
+                }
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            //取消所有任务
+            for (Future<Integer> f : futures) {
+                f.cancel(true);
+            }
+        }
+
+        // 返回结果
+        System.out.println("result = " + r);
+```
+
 ## 5 总结
+
+当需要批量提交异步任务的时候建议你使用 CompletionService。CompletionService 将线程池 Executor 和阻塞队列 BlockingQueue 的功能融合在了一起，能够让批量异步任务的管理更简单。除此之外，CompletionService 能够让异步任务的执行结果有序化，先执行完的先进入阻塞队列，利用这个特性，你可以轻松实现后续处理的有序性，避免无谓的等待，同时还可以快速实现诸如 Forking Cluster 这样的需求。
+
+CompletionService 的实现类 ExecutorCompletionService，需要你自己创建线程池，虽看上去有些啰嗦，但好处是你可以让多个 ExecutorCompletionService 的线程池隔离，这种隔离性能避免几个特别耗时的任务拖垮整个应用的风险。
 
 ## 6 思考题
 
+本节示例使用 CompletionService 实现了一个询价应用的核心功能，后来又有了新的需求，需要计算出最低报价并返回，下面的示例代码尝试实现这个需求，你看看是否存在问题呢？
+
+```java
+        // 创建线程池
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        // 创建CompletionService
+        CompletionService<Integer> cs = new ExecutorCompletionService<>(executor);
+        // 异步向电商S1询价
+        cs.submit(() -> getPriceByS1());
+        // 异步向电商S2询价
+        cs.submit(() -> getPriceByS2());
+        // 异步向电商S3询价
+        cs.submit(() -> getPriceByS3());
+        // 将询价结果异步保存到数据库
+        // 并计算最低报价
+        AtomicReference<Integer> m = new AtomicReference<>(Integer.MAX_VALUE);
+        for (int i = 0; i < 3; i++) {
+            executor.execute(() -> {
+                Integer r = null;
+                try {
+                    r = cs.take().get();
+                } catch (Exception e) {
+                }
+                save(r);
+                m.set(Integer.min(m.get(), r));
+            });
+        }
+
+        return m;
+```
+
+- `return m;` 等不到任务执行完成就会返回。
+- AtomicReference 的 set 和 get 不是原子操作，这里存在数据竞争。
