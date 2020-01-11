e44e8888de56058e6efeffe3983240ac5ba53df6
Ztiany <Ztiany3@gmail.com>
2020-01-11 19:42:51 +0800

    update kotlin

--- Kotlin/Code/Kotlin-Coroutines/src/main/kotlin/retrofit/RetrofitMockMain.kt
+++ Kotlin/Code/Kotlin-Coroutines/src/main/kotlin/retrofit/RetrofitMockMain.kt
@@ -25,7 +25,7 @@ private val retrofit = Retrofit.Builder().apply {
 
 private val githubApi = retrofit.create(GitHubRetrofit::class.java)
 
-fun main() = sample4()
+fun main() = sample2()
 
 /*  如果是 RxJava 处理 Retrofit 结果，那么如果服务器 body 返回空，对于 Flowable 来说，只会调用 onCompleted，此时可以使用 compose 和 switchIfEmpty API。 */
 fun sample4() {
@@ -61,7 +61,7 @@ fun sample3() {
     Thread.sleep(3000)
 }
 
-/*目前来看，retrofit 接口中的 suspend 方法不支持返回 T?，返回注诸如 204 之类响应将会导致 Response from xxx was null but response body type was declared as non-null KotlinNullPointerException 异常*/
+/*目前来看，retrofit 接口中的 suspend 方法不支持返回 T?，返回注诸如 204 之类响应将会导致 kotlin.KotlinNullPointerException: Response from xxx was null but response body type was declared as non-null KotlinNullPointerException 异常*/
 private fun sample2() = runBlocking {
     try {
         val mock2 = githubApi.mock1()
